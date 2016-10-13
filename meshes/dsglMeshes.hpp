42f16a53a565cd0d135070061719437d327533d7
DenisSalem <denissalem@tuxfamily.org>
2016-10-13 10:35:26 +0200

    nouveau fichier : meshes/dsglMeshes.hpp

---
+++ meshes/dsglMeshes.hpp
@@ -0,0 +1,16 @@
+#if defined(DSGL_GL3W)
+	#include <GL/gl3w.h>
+#else
+	#include <GL/gl3w.h>
+#endif
+
+struct Triangle {
+	GLfloat		vertex[9];
+	GLuint		index[3];.0, 0.0,
+				-1.0, -1.0, 0.0,
+				1.0, -1.0, 0.0
+			},
+			{0,1,2}
+		};
+	}
+}
