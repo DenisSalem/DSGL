be1fc637fba376d9912a8535ba3734aa0e4130e7
DenisSalem <denissalem@tuxfamily.org>
2016-10-25 13:25:46 +0200

    nouveau fichier : FirstTriangle/Makefile 	nouveau fichier : FirstTriangle/dsgl.cpp 	nouveau fichier : FirstTriangle/dsgl.hpp 	nouveau fichier : FirstTriangle/dsglMeshes.cpp 	nouveau fichier : FirstTriangle/dsglMeshes.hpp 	nouveau fichier : FirstTriangle/fragment.shader 	nouveau fichier : FirstTriangle/main.cpp 	nouveau fichier : FirstTriangle/vertex.shader 	modifié :         Instancing/main.cpp

--- examples/FirstTriangle/main.cpp
+++ examples/FirstTriangle/main.cpp
@@ -1,47 +1,26 @@
 #include "dsgl.hpp"
 #include "dsglMeshes.hpp"
-
-#include <sstream>
 #include <unistd.h>
-
-#include <glm/glm.hpp>
 
 int main() {
 	/* Init Context and create shader program used to render our instances. */
 
-	DSGL::Context context("INSTANCING QUADS WITH DSGL", 640, 480, 4, 3);
+	DSGL::Context context("DSGL HELLO TRIANGLE",640,480,4,3);
 	
 	context.InitSimpleWindow();
 	
 	DSGL::ShaderProgram shaderProgram("vertex.shader","fragment.shader");
 
-	/* Define our mesh, a quad for instance. */
-	DSGL::Meshes::Quad quad;
-
-	/* Define instances */
-	glm::vec2 translations[100];
-	int index = 0;
-	GLfloat offset = 0.1f;
-	for(GLint y = -10; y < 10; y += 2) {
-		for(GLint x = -10; x < 10; x += 2) {
-			glm::vec2 translation;
-			translation.x = (GLfloat) x / 10.0f + offset;
-			translation.y = (GLfloat) y / 10.0f + offset;
-			translations[index++] = translation;
-		}
-	}  
+	/* DSGL provide built in simple triangle for playing and debugging. */
+	DSGL::Meshes::Triangle triangle;
 
 	/* Create buffers */
-	DSGL::Elements elements(4 * sizeof(GLuint), quad.index);
-	DSGL::VertexBufferObject VBO(sizeof(GLfloat) * 12, quad.vertex);
-	DSGL::VertexBufferObject instances(sizeof(glm::vec2) * 100, translations);
-	DSGL::VertexArrayObject VAO(elements.ID, VBO.ID, instances.ID);
+	DSGL::Elements elements(3 * sizeof(GLuint), triangle.index);
+	DSGL::VertexBufferObject VBO(sizeof(GLfloat) * 9, triangle.vertex);
+	DSGL::VertexArrayObject VAO(elements.ID, VBO.ID);
 	
 	/* Set up how vertex related memory is organized */
 	VAO.AttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), 0);
-
-	/* Set up how instances memory is organized */
-	VAO.InstancesAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(GLfloat), 0, 1);
 
 	glEnable(GL_CULL_FACE);
 	
@@ -60,7 +39,7 @@ int main() {
 
 		usleep(40000); /* Let the GPU take a breath */
 
-		glDrawElementsInstanced(GL_TRIANGLE_STRIP, 4, GL_UNSIGNED_INT, (GLvoid *) 0, 100);
+		glDrawElements(GL_TRIANGLE_STRIP, 4, GL_UNSIGNED_INT, (GLvoid *) 0);
 
 		glBindBuffer(GL_ARRAY_BUFFER, 0); /* Optional since we're using only one VAO */
 
